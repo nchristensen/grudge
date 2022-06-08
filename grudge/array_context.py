447626c5fe03d1a0283f0acf8c23cdc9510665ec
Nicholas Christensen <nicholas.j.christensen@outlook.com>
2022-06-08 19:03:03 -0400

    Change asterisk placement

--- grudge/array_context.py
+++ grudge/array_context.py
@@ -314,7 +314,7 @@ class _DistributedCompiledFunction:
 
 class MPIPytatoArrayContextBase(MPIBasedArrayContext):
     def __init__(
-            self, mpi_communicator, queue, *, mpi_base_tag, allocator=None
+            self, mpi_communicator, queue, mpi_base_tag, *, allocator=None
             ) -> None:
         if allocator is None:
             warn("No memory allocator specified, please pass one. "
