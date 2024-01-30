5600c564d06430aad0da7d843e196514de4a1a2f
Nicholas Christensen <njchris2@illinois.edu>
2024-01-30 14:47:16 -0600

    Switch to Autotuning fusion contractor array context

--- grudge/array_context.py
+++ grudge/array_context.py
@@ -507,12 +507,11 @@ if _HAVE_FUSION_ACTX:
     MPIPytatoArrayContext = MPIFusionContractorArrayContext
 
     # Need to fix KernelDumpingFusionContractorArrayContext so can run with fuxed kernels
-    from meshmode.array_context import KernelDumpingFusionContractorArrayContextOld#Base#AutotuningFusionContractorArrayContext
-    class MPIKernelDumpingFusionContractorArrayContext(MPIPytatoArrayContextBase, KernelDumpingFusionContractorArrayContextOld):#Base):
+    from meshmode.array_context import KernelDumpingFusionContractorArrayContextOld, AutotuningFusionContractorArrayContext
+    class MPIKernelDumpingFusionContractorArrayContext(MPIPytatoArrayContextBase, AutotuningFusionContractorArrayContext):#KernelDumpingFusionContractorArrayContextOld):#AutotuningFusionContractorArrayContext):#KernelDumpingFusionContractorArrayContextOld):#Base):
         """
         ..autofunction:: __init__
         """
-
 
     MPIPytatoArrayContext = MPIKernelDumpingFusionContractorArrayContext
     
